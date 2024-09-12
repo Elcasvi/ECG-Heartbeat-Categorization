722e0f501bf28b0c6c83adeea4b9e811e205510b
Fabian Lioner <lionerrochae@gmail.com>
2024-09-11 20:36:21 -0600

    charting

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,7 +1,65 @@
-import Projects from "./componetns/projects/projects";
+"use client";
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import data from "./componetns/datos/data.json"; // Adjust the path as necessary
 import s from "./page.module.css";
+import Projects from "./componetns/projects/projects";
+
+interface ChartData {
+  labels: number[];
+  datasets: {
+    label: string;
+    data: number[];
+    borderColor: string;
+    backgroundColor: string;
+    borderWidth: number;
+  }[];
+}
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function Home() {
+  const [chartData, setChartData] = useState<ChartData | null>(null);
+
+  useEffect(() => {
+    // Extract the first array from the data
+    const firstArray = data.data[0];
+
+    // Prepare chart data
+    const chartData: ChartData = {
+      labels: firstArray.map((_, index) => index + 1), // X-axis labels as index + 1
+      datasets: [
+        {
+          label: "ECG Data",
+          data: firstArray,
+          borderColor: "rgba(75, 192, 192, 1)",
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    setChartData(chartData);
+  }, []);
+
   return (
     <div className={s.mainCont}>
       <div className={s.title}>ECG Heartbeat Categorization</div>
@@ -14,6 +72,12 @@ export default function Home() {
         fin de reducir las tasas de mortalidad y mejorar la atención médica.
       </div>
       <Projects />
+      {chartData && (
+        <div>
+          <h2>ECG Data Graph</h2>
+          <Line data={chartData} />
+        </div>
+      )}
     </div>
   );
 }
