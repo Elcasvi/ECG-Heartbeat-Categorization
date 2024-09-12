908d588bc38a02f277c1a597845476f8889e3e5e
Fabian Lioner <lionerrochae@gmail.com>
2024-09-12 15:17:30 -0600

    api connection

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -6,22 +6,79 @@ import s from "./page.module.css";
 import Projects from "./components/projects/projects";
 
 export default function Home() {
+  const categories = [
+    {
+      name: "Normal",
+    },
+    {
+      name: "Supraventriculares",
+    },
+    {
+      name: "Desconocidos",
+    },
+    {
+      name: "Ventriculares",
+    },
+    {
+      name: "Fusión",
+    },
+  ];
+
+  const categoryMapping: { [key: string]: string } = {
+    S: categories[1].name, // Supraventriculares
+    Q: categories[2].name, // Desconocidos
+    V: categories[3].name, // Ventriculares
+    F: categories[4].name, // Fusión
+  };
+
   const [chartData, setChartData] = useState<{ x: number; y: number }[] | null>(
     null
   );
+  const [selectedCategory, selectCategory] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [responseData, setResponseData] = useState(null);
 
-  useEffect(() => {
-    // Extract the first array from the data
-    const firstArray = data.data[0];
+  const apiEndpoint =
+    "https://ecg-classifier-app-ctang6ahckcdb8gd.eastus-01.azurewebsites.net/predict_illness";
 
-    // Prepare chart data
-    const formattedData = firstArray.map((value, index) => ({
-      x: index + 1, // X-axis as index + 1
+  const formatData = (index: number) => {
+    const formattedData = data.data[index].map((value, index) => ({
+      x: index + 1,
       y: value,
     }));
+    return formattedData;
+  };
 
-    setChartData(formattedData);
+  useEffect(() => {
+    setChartData(formatData(0));
   }, []);
+
+  const handleCategoryClick = (index: number) => {
+    setChartData(formatData(index));
+    selectCategory(index);
+  };
+
+  const handleApiCall = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(apiEndpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: data.data[selectedCategory] }),
+      });
+      if (response.ok) {
+        const result = await response.json();
+        setResponseData(result);
+      } else {
+        console.error("API request failed:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error during API request:", error);
+    }
+    setLoading(false);
+  };
 
   return (
     <div className={s.mainCont}>
@@ -35,9 +92,26 @@ export default function Home() {
         fin de reducir las tasas de mortalidad y mejorar la atención médica.
       </div>
       <Projects />
+
+      <div className={s.subtitle}>Selecciona una muestra para analizaar</div>
+      <div className={s.categories}>
+        {categories
+          .filter((category) => category.name.toLowerCase() !== "normal")
+          .map((category, index) => (
+            <div
+              key={index}
+              onClick={() => handleCategoryClick(index + 1)}
+              className={
+                selectedCategory != index + 1 ? s.category : s.categorySelected
+              }
+            >
+              {category.name}
+            </div>
+          ))}
+      </div>
+
       {chartData && (
         <div>
-          <h2>ECG Data Graph</h2>
           <VictoryChart width={700} theme={VictoryTheme.material}>
             <VictoryAxis
               tickFormat={(x: { toString: () => any }) => x.toString()}
@@ -53,8 +127,27 @@ export default function Home() {
               style={{
                 data: { stroke: "rgba(255, 0, 0, 1)" },
               }}
+              animate={{
+                duration: 1000, // Animation duration in milliseconds
+                onLoad: { duration: 1000 }, // Initial loading animation
+                easing: "linear", // Type of easing function (e.g., "linear", "bounce", etc.)
+              }}
             />
           </VictoryChart>
+        </div>
+      )}
+      <button onClick={handleApiCall} disabled={loading} className={s.category}>
+        {loading ? "Loading..." : "Analizar datos con AdaBoost"}
+      </button>
+      {responseData && (
+        <div className={s.resultCont}>
+          <div>{"Resultado de la API "}</div>
+          <div>
+            {" "}
+            {responseData}
+            {": "}
+            {categoryMapping[responseData]}
+          </div>
         </div>
       )}
     </div>
