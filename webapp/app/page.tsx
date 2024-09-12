4a7a8d911dadae8f94d3d3d3bd67086044b4e615
Fabian Lioner <lionerrochae@gmail.com>
2024-09-11 21:01:15 -0600

    victory migration

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,63 +1,26 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import data from "./componetns/datos/data.json"; // Adjust the path as necessary
+import { VictoryChart, VictoryLine, VictoryAxis, VictoryTheme } from "victory";
+import data from "./components/datos/data.json";
 import s from "./page.module.css";
-import Projects from "./componetns/projects/projects";
-
-interface ChartData {
-  labels: number[];
-  datasets: {
-    label: string;
-    data: number[];
-    borderColor: string;
-    backgroundColor: string;
-    borderWidth: number;
-  }[];
-}
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import Projects from "./components/projects/projects";
 
 export default function Home() {
-  const [chartData, setChartData] = useState<ChartData | null>(null);
+  const [chartData, setChartData] = useState<{ x: number; y: number }[] | null>(
+    null
+  );
 
   useEffect(() => {
     // Extract the first array from the data
     const firstArray = data.data[0];
 
     // Prepare chart data
-    const chartData: ChartData = {
-      labels: firstArray.map((_, index) => index + 1), // X-axis labels as index + 1
-      datasets: [
-        {
-          label: "ECG Data",
-          data: firstArray,
-          borderColor: "rgba(75, 192, 192, 1)",
-          backgroundColor: "rgba(75, 192, 192, 0.2)",
-          borderWidth: 1,
-        },
-      ],
-    };
+    const formattedData = firstArray.map((value, index) => ({
+      x: index + 1, // X-axis as index + 1
+      y: value,
+    }));
 
-    setChartData(chartData);
+    setChartData(formattedData);
   }, []);
 
   return (
@@ -66,7 +29,7 @@ export default function Home() {
       <div className={s.desc}>
         Este trabajo aborda el desarrollo de un modelo de clasificación para
         identificar diversas afecciones cardíacas a partir de lecturas de
-        electro- cardiogramas (ECG). Dado que las enfermedades cardiovasculares
+        electrocardiogramas (ECG). Dado que las enfermedades cardiovasculares
         son la principal causa de mortalidad en México, este proyecto se centra
         en mejorar la detección temprana de condiciones como arritmias, con el
         fin de reducir las tasas de mortalidad y mejorar la atención médica.
@@ -75,7 +38,23 @@ export default function Home() {
       {chartData && (
         <div>
           <h2>ECG Data Graph</h2>
-          <Line data={chartData} />
+          <VictoryChart theme={VictoryTheme.material}>
+            <VictoryAxis
+              tickFormat={(x: { toString: () => any }) => x.toString()}
+            />
+            <VictoryAxis
+              dependentAxis
+              tickFormat={(y: number) => y.toFixed(2)}
+            />
+            <VictoryLine
+              data={chartData}
+              x="x"
+              y="y"
+              style={{
+                data: { stroke: "rgba(75, 192, 192, 1)" },
+              }}
+            />
+          </VictoryChart>
         </div>
       )}
     </div>
